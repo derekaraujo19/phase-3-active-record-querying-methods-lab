1184ab6386ca2ee2b792f368ccd3eae826f3b10b
Derek Araujo <derekaraujo19@gmail.com>
2022-11-03 21:46:16 -0400

    commit tree f3e43e00643b79887c304f46248c7dcdb2d91ef3

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
+
   def self.highest_rating
-    Show.maximum(:rating)
+     Show.maximum(:rating)
   end
 
   def self.most_popular_show
@@ -13,8 +14,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    lowest_rating = Show.lowest_rating
-    Show.find_by(rating: lowest_rating)
+    least = Show.lowest_rating
+    Show.find_by(rating: least)
   end
 
   def self.ratings_sum
@@ -22,11 +23,143 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-    Show.where('rating > 5')
+    Show.where("rating > ?", 5)
   end
 
   def self.shows_by_alphabetical_order
-    Show.order(:name)
+    Show.order(name: :asc)
   end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # def self.highest_rating
+  #   Show.maximum(:rating)
+  # end
+
+  # def self.most_popular_show
+  #   highest_rating = Show.highest_rating
+  #   Show.find_by(rating: highest_rating)
+  # end
+
+  # def self.lowest_rating
+  #   Show.minimum(:rating)
+  # end
+
+  # def self.least_popular_show
+  #   lowest_rating = Show.lowest_rating
+  #   Show.find_by(rating: lowest_rating)
+  # end
+
+  # def self.ratings_sum
+  #   Show.sum(:rating)
+  # end
+
+  # def self.popular_shows
+  #   Show.where("rating > 5")
+  # end
+
+  # def self.shows_by_alphabetical_order
+  #   Show.order(name: :asc)
+  # end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # def self.highest_rating
+  #   Show.maximum(:rating)
+  # end
+
+  # def self.most_popular_show
+  #   highest_rating = Show.highest_rating
+  #   Show.find_by(rating: highest_rating)
+  # end
+
+  # def self.lowest_rating
+  #   Show.minimum(:rating)
+  # end
+
+  # def self.least_popular_show
+  #   lowest_rating = Show.lowest_rating
+  #   Show.find_by(rating: lowest_rating)
+  # end
+
+  # def self.ratings_sum
+  #   Show.sum(:rating)
+  # end
+
+  # def self.popular_shows
+  #   Show.where('rating > 5')
+  # end
+
+  # def self.shows_by_alphabetical_order
+  #   Show.order(:name)
+  # end
+
 end
+
+# bundle exec rake db:create_migration NAME=add_season_to_shows
+
+# bundle exec rake db:migrate
